096225631e03bd295ff665f68b0a45d88106bdc4
Avinash Dasoundhi <avi.avinash3008@gmail.com>
2024-01-04 13:39:40 +0530

    4th Jan 24

---
+++ Medium/560-SubarrayKSum.py
@@ -0,0 +1,42 @@
+"""
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+
+
+Constraints:
+
+1 <= nums.length <= 2 * 10^4
+-1000 <= nums[i] <= 1000
+-10^7 <= k <= 10^7
+"""
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        if len(nums) == 0:
+            return 0
+        d = dict()
+        cur_sum = 0
+        count = 0
+        for i in nums:
+            cur_sum += i
+            if cur_sum == k:
+                count += 1
+            if cur_sum - k in d:
+                count += d[cur_sum - k]
+            if cur_sum in d:
+                d[cur_sum] += 1
+            else:
+                d[cur_sum] = 1
+        return count
---
+++ Medium/974-SubarraySumsDivByK.py
@@ -0,0 +1,37 @@
+"""
+Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+
+A subarray is a contiguous part of an array.
+
+
+
+Example 1:
+
+Input: nums = [4,5,0,-2,-3,1], k = 5
+Output: 7
+Explanation: There are 7 subarrays with a sum divisible by k = 5:
+[4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+Example 2:
+
+Input: nums = [5], k = 9
+Output: 0
+
+
+Constraints:
+
+1 <= nums.length <= 3 * 10^4
+-10^4 <= nums[i] <= 10^4
+2 <= k <= 10^4
+"""
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        d = {0: 1}
+        prefix_sum, count = 0, 0
+        for num in nums:
+            prefix_sum = (prefix_sum + num) % k
+            if prefix_sum < 0:
+                prefix_sum += k
+            count += d.get(prefix_sum, 0)
+            d[prefix_sum] = d.get(prefix_sum, 0) + 1
+        return count
