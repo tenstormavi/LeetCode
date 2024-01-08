9ae159acc36cfd344065c21752c1225574e1e926
Avinash Dasoundhi <avi.avinash3008@gmail.com>
2024-01-08 23:33:01 +0530

    8th Jan 2024

---
+++ Medium/75-SortColors.py
@@ -0,0 +1,48 @@
+"""
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects
+of the same color are adjacent, with the colors in the order red, white, and blue.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+You must solve this problem without using the library's sort function.
+
+Example 1:
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Constraints:
+    n == nums.length
+    1 <= n <= 300
+    nums[i] is either 0, 1, or 2.
+"""
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # Approach: O(n)
+        low = medium = 0
+        high = len(nums) - 1
+        while medium <= high:
+            if nums[medium] == 0:
+                # swap low and medium
+                nums[low], nums[medium] = nums[medium], nums[low]
+                low += 1
+                medium += 1
+            elif nums[medium] == 1:
+                # do nothing just move forward
+                medium += 1
+            else:
+                # swap medium and high
+                nums[medium], nums[high] = nums[high], nums[medium]
+                high -= 1
+
+        # Approach: Using sorting - Merge Sort (O(nLogn)), Bubble sort(O(n^2))
+        # for i in range(len(nums)):
+        #     for j in range(0, len(nums)-i-1):
+        #         if nums[j] > nums[j+1]:
+        #             nums[j], nums[j+1] = nums[j+1], nums[j]
