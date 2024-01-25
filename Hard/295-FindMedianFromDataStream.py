eb774628a7a8c9788b1be9a3b908c90b7316a2bc
Avinash Dasoundhi <avi.avinash3008@gmail.com>
2024-01-25 22:22:11 +0530

    25th Jan 2024

--- Hard/295-FindMedianFromDataStream.py
+++ Hard/295-FindMedianFromDataStream.py
@@ -1,5 +1,39 @@
 """
+The median is the middle value in an ordered integer list. If the size of the list is even,
+there is no middle value, and the median is the mean of the two middle values.
+    For example, for arr = [2,3,4], the median is 3.
+    For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
 
+Implement the MedianFinder class:
+    MedianFinder() initializes the MedianFinder object.
+    void addNum(int num) adds the integer num from the data stream to the data structure.
+    double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual
+    answer will be accepted.
+
+
+Example 1:
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0
+
+Constraints:
+    -10^5 <= num <= 10^5
+    There will be at least one element in the data structure before calling findMedian.
+    At most 5 * 104 calls will be made to addNum and findMedian.
+
+Follow up:
+    If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+    If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
 """
 
 
@@ -10,7 +44,7 @@ class MedianFinder:
         # which can worst take O(n), to reduce the time can use 2 heaps.
 
         # Small - maxHeap, Large - minHeap O(log(n))
-        # Conditions: heaps should be equal size
+        # Conditions: heaps should be roughly equal size
         self.small, self.large = [], []
 
     def addNum(self, num: int) -> None:
@@ -18,7 +52,7 @@ class MedianFinder:
         heappush(self.small, -1 * num)
 
         # make sure every num in small is <= every num in large
-        if (small and large and (-1 * self.small[0]) > self.large[0]):
+        if (self.small and self.large and (-1 * self.small[0]) > self.large[0]):
             val = -1 * heappop(self.small)
             heappush(self.large, val)
 
@@ -29,6 +63,16 @@ class MedianFinder:
         if len(self.large) > len(self.small) + 1:
             val = heappop(self.large)
             heappush(self.small, -1 * val)
+
+    def findMedian(self) -> float:
+        # Odd length
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        if len(self.large) > len(self.small):
+            return self.large[0]
+
+        # Even length
+        return (-1 * self.small[0] + self.large[0]) / 2
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
